78e898b0946f59ddb5153ad22a42fd1d224970a4
Ankit <118896790+ASR-047@user.noreply.github.com>
2025-05-13 18:45:21 +0530

    setup parcel & tailwind

---
+++ README.md
@@ -0,0 +1,14 @@
+# features
+  - Login/Sign Up
+- Sign in/ Sign up Form
+- redirect to browse page
+- Browse (after authentication)
+- Header
+- Main Movie
+    - Trailer in background
+    - Title & description
+    - Movie suggestion
+        - movie lists * n
+# Netflix-GPT
+- Searchbar
+- Movie suggestion
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+//const tailwind = require('tailwindcss');
+// const autoprefixer = require('autoprefixer');
+
+// module.exports = {
+//   plugins: [
+//     tailwind,
+//     autoprefixer,
+//   ],
+// };
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
