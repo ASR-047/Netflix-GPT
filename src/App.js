76846ce06cc72a8a0c93f085cd662290737364eb
Ankit <118896790+ASR-047@user.noreply.github.com>
2025-05-13 18:38:37 +0530

    Remove node_modules from Git

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <p>Hello World</p>
+            </header>
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
